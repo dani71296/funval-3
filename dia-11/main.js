0a91bc29dc7add1fee6ca6d397d438643975a628
Daniel <isral.tudela712.96@gmail.com>
2025-06-03 18:18:43 -0400

    dia 11 clase

---
+++ dia-11/main.js
@@ -0,0 +1,45 @@
+//comentario de daniel en una linea
+
+/*comentariio
+milti linea
+de Daniel
+tudela
+pacheco
+Israel
+*/
+// Tipos de datos primitivos en JavaScript
+
+// 1. String (cadena de texto)
+let nombre = "daniel";
+
+// 2. Number (número - enteros o decimales)
+let edad = 28;
+let precio = 32.99;
+
+// 3. Boolean (booleano - verdadero o falso)
+let esEstudiante = true;
+
+// 4. Undefined (no definido)
+let direccion; // aún no tiene valor
+
+// 5. Null (nulo - intencionadamente vacío)
+let telefono = null;
+
+// 6. Symbol (símbolo - valor único e inmutable)
+let id = Symbol("&&");
+
+// 7. BigInt (número entero muy grande)
+let numeroGrande = 1234567890123456789012345678901234567890n;
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log( nombre, "| Tipo:", typeof nombre);
+console.log( edad, "| Tipo:", typeof edad);
+console.log( precio, "| Tipo:", typeof precio);
+console.log( esEstudiante, "| Tipo:", typeof esEstudiante);
+console.log( direccion, "| Tipo:", typeof direccion);
+console.log( telefono, "| Tipo:", typeof telefono);
+console.log( id.toString(), "| Tipo:", typeof id);
+console.log( numeroGrande, "| Tipo:", typeof numeroGrande);
+
+
+
