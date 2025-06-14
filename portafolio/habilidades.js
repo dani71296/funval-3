bed12e391b1a18853c2e5ad584a1b57fc992ba1f
Daniel <isral.tudela712.96@gmail.com>
2025-06-13 21:24:38 -0400

    portafolio fase 5

--- portafolio/habilidades.js
+++ portafolio/habilidades.js
@@ -1,42 +1,52 @@
 const habilidades = [
   {
     nombre: "Lenguaje C",
-    imagen: "./images/selogo.jpg"
+    imagen: "./images/selogo.jpg",
+    descripcion:"Util para programar sistemas operativos, software embebido, compiladores, sistemas de bases de datos, juegos, y mucho más"
   },
   {
     nombre: "SolidWorks",
-    imagen: "./images/solidworklogo.jpg"
+    imagen: "./images/solidworklogo.jpg",
+    descripcion:"Util para modelar piezas y ensamblajes en 3D, así como para generar dibujos en 2D",
   },
   {
     nombre: "Python",
-    imagen: "./images/Pythonlogo.png"
+    imagen: "./images/Pythonlogo.png",
+    descripcion:"Util para desarrollar aplicaciones, sitios web, software, y también para automatizar tareas, analizar datos y realizar tareas de ciencia de datos y machine learning",
   },
   {
     nombre: "JavaScript",
-    imagen: "./images/JavaScriptlogo.png"
+    imagen: "./images/JavaScriptlogo.png",
+    descripcion:" Util para añadir interactividad y dinamismo a las páginas web",
   },
   {
     nombre: "Autodesk Inventor",
-    imagen: "./images/inventorlogo.png"
+    imagen: "./images/inventorlogo.png",
+    descripcion:"Software de diseño 3D que se utiliza para el diseño mecánico, la documentación y la simulación de productos",
   },
   {
     nombre: "HTML",
-    imagen: "./images/htmlog.png"
+    imagen: "./images/htmlog.png",
+    descripcion:"Lenguaje de marcado que se utiliza para estructurar y organizar el contenido de las páginas web",
   },
   {
     nombre: "Excel",
-    imagen: "./images/excelogo.jpg"
+    imagen: "./images/excelogo.jpg",
+    descripcion:"Util para organizar, analizar y visualizar datos",
   },
   {
     nombre: "CSS",
-    imagen: "./images/csslog.jpg"
+    imagen: "./images/csslog.jpg",
+    descripcion:"Lenguaje de programación utilizado para definir el estilo visual de un documento web",
   },
   {
     nombre: "Arduino",
-    imagen: "./images/arduiniolog.png"
+    imagen: "./images/arduiniolog.png",
+    descripcion:"Util para crear dispositivos electrónicos interactivos, robots, sistemas domóticos y proyectos de investigación, entre otros",
   },
   {
     nombre: "3D Modeling",
-    imagen: "./images/3dlogos2.jpg"
+    imagen: "./images/3dlogos2.jpg",
+    descripcion:"El modelado 3D util para crear representaciones digitales tridimensionales de objetos o formas",
   }
 ];
--- portafolio/script.js
+++ portafolio/script.js
@@ -1,10 +1,32 @@
 function mostrarHabilidades() {
   const contenedor = document.getElementById('contenedor-habilidades');
+  
   habilidades.forEach(habilidad => {
+    // Crear contenedor
     const div = document.createElement('div');
-    div.innerHTML = `
-      <img src="${habilidad.imagen}" alt="${habilidad.nombre}" title="${habilidad.nombre}"
-        class="h-20 object-contain rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-120">`;
+    div.className = "flex justify-center"; // opcional: centra la imagen
+
+    // Crear imagen
+    const img = document.createElement('img');
+    img.src = habilidad.imagen;
+    img.alt = habilidad.nombre;
+    img.title = habilidad.nombre;
+    img.className = "h-20 object-contain rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-110 cursor-pointer";
+
+    // Evento al hacer click
+    img.addEventListener("click", () => {
+      document.getElementById("modal-img").src = habilidad.imagen;
+      document.getElementById("modal-titulo").textContent = habilidad.nombre;
+      document.getElementById("modal-desc").textContent = habilidad.descripcion;
+      document.getElementById("modal").classList.remove("hidden");
+    });
+    //evento para cerrar
+    document.getElementById("cerrar-modal").addEventListener("click", () => {
+    document.getElementById("modal").classList.add("hidden");
+  });
+
+    // Insertar imagen en contenedor y contenedor al DOM
+    div.appendChild(img);
     contenedor.appendChild(div);
   });
 }
